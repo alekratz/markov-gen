683d64897f4281959b60e3967a2ea5774f2063c3
Alek Ratzloff <alekratz@gmail.com>
2015-01-05 16:14:36 -0500

    Removed underscores from books, added capability to fix.sh

--- text/fix.sh
+++ text/fix.sh
@@ -4,7 +4,8 @@ for f in $(find . -type f -name \*.txt); do
   echo "applying changes $f"
   sed "s/\([a-zA-Z!?\.]\)--/\1 --/g" $f.new1 > $f.new2
   sed "s/--\([a-zA-Z!?\.]\)/-- \1/g" $f.new2 > $f.new3
+  sed "s/_//g" $f.new3 > $f.new4
 
-  mv $f.new3 $f
-  rm $f.new{1,2}
+  mv $f.new4 $f
+  rm $f.new{1,2,3}
 done
