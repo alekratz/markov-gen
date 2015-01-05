4cb3ca9cefa1e5622c83a30d198822dd5ce07edd
Alek Ratzloff <alekratz@gmail.com>
2015-01-05 11:31:15 -0500

    Add a tool that will fix the dashes in the texts.

---
+++ text/fix.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for f in $(find . -type f -name \*.txt); do
+  cp $f $f.new1
+  echo "applying changes $f"
+  sed "s/\(.\)--/\1 --/g" $f.new1 > $f.new2
+  sed "s/--\(.\)/-- \1/g" $f.new2 > $f.new3
+
+  mv $f.new3 $f
+  rm $f.new{1,2}
+done
