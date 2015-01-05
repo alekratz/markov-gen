0f2ef8215cae78c841dd9046c4ab956bf8a2627e
Alek Ratzloff <alekratz@gmail.com>
2015-01-05 11:35:00 -0500

    Made it so that fix.sh will not add extra spaces if run on the same file twice

--- text/fix.sh
+++ text/fix.sh
@@ -2,8 +2,8 @@
 for f in $(find . -type f -name \*.txt); do
   cp $f $f.new1
   echo "applying changes $f"
-  sed "s/\(.\)--/\1 --/g" $f.new1 > $f.new2
-  sed "s/--\(.\)/-- \1/g" $f.new2 > $f.new3
+  sed "s/\([a-zA-Z!?\.]\)--/\1 --/g" $f.new1 > $f.new2
+  sed "s/--\([a-zA-Z!?\.]\)/-- \1/g" $f.new2 > $f.new3
 
   mv $f.new3 $f
   rm $f.new{1,2}
